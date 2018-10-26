537a0f375a8d50bc05b10b3bc5bd6b16fe84448d
Julio Rodrigues <juliorodrigues@MacBook-Pro-de-Julio.local>
2018-10-26 16:43:58 -0300

    Implementação da autorização e autenticação por tokens JWT.

--- src/main/java/br/com/julio/pontointeligente/api/controllers/FuncionarioController.java
+++ src/main/java/br/com/julio/pontointeligente/api/controllers/FuncionarioController.java
@@ -1,4 +1,118 @@
 package br.com.julio.pontointeligente.api.controllers;
 
+import br.com.julio.pontointeligente.api.dtos.FuncionarioDto;
+import br.com.julio.pontointeligente.api.entities.Funcionario;
+import br.com.julio.pontointeligente.api.response.Response;
+import br.com.julio.pontointeligente.api.services.FuncionarioService;
+import br.com.julio.pontointeligente.api.utils.PasswordUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/funcionarios")
+@CrossOrigin(origins = "*")
 public class FuncionarioController {
+
+    private static  final Logger log = LoggerFactory.getLogger(FuncionarioController.class);
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    public FuncionarioController() {
+    }
+
+    /**
+     * Atualiza os dados de um funcionário.
+     *
+     * @param id
+     * @param funcionarioDto
+     * @param result
+     * @return ResponseEntity<Response<FuncionarioDto>>
+     * @throws Exception
+     */
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Response<FuncionarioDto>> atualizar(@PathVariable("id") Long id,
+                                                              @Valid @RequestBody FuncionarioDto funcionarioDto,
+                                                              BindingResult result) throws Exception {
+        log.info("Atualizando Funcionario: {}", funcionarioDto.toString());
+        Response<FuncionarioDto> response = new Response<>();
+
+        Optional<Funcionario> funcionario = this.funcionarioService.buscarPorId(id);
+        if (!funcionario.isPresent()){
+            result.addError(new ObjectError("funcionario", "Funcioário não encontrado."));
+        }
+
+        this.atualizaDadosFuncionario(funcionario.get(), funcionarioDto, result);
+
+        if(result.hasErrors()) {
+            log.error("Erro validando funcioário: {}", result.getAllErrors());
+            result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        this.funcionarioService.persistir(funcionario.get());
+        response.setData(this.converterFuncionarioDto(funcionario.get()));
+
+        return ResponseEntity.ok(response);
+
+    }
+
+    /**
+     * Retorna um DTO com os dados de um funcionário.
+     *
+     * @param funcionario
+     * @return FuncionarioDto
+     */
+    private FuncionarioDto converterFuncionarioDto(Funcionario funcionario) {
+        FuncionarioDto funcionarioDto = new FuncionarioDto();
+        funcionarioDto.setId(funcionario.getId());
+        funcionarioDto.setEmail(funcionario.getEmail());
+        funcionarioDto.setNome(funcionario.getNome());
+        funcionario.getQtdHorasAlmocoOpt().ifPresent(qtdHorasAlmoco -> funcionarioDto.setQtdHorasAlmoco(Optional.of(Float.toString(qtdHorasAlmoco))));
+        funcionario.getQtdHorasTrabalhoDiaOpt().ifPresent(qtdHorasTrabDia -> funcionarioDto.setQtdHorasTrabalhoDia(Optional.of(Float.toString(qtdHorasTrabDia))));
+        funcionario.getValorHoraOpt().ifPresent(valorHora -> funcionarioDto.setValorHora(Optional.of(valorHora.toString())));
+
+        return funcionarioDto;
+    }
+
+    /**
+     * Atualiza os dados do funcionário com base nos dados encontrados no DTO.
+     *
+     * @param funcionario
+     * @param funcionarioDto
+     * @param result
+     * @throws NoSuchAlgorithmException
+     */
+    private void atualizaDadosFuncionario(Funcionario funcionario, FuncionarioDto funcionarioDto, BindingResult result) throws NoSuchAlgorithmException {
+        funcionario.setNome(funcionarioDto.getNome());
+
+        if(!funcionario.getEmail().equals(funcionarioDto.getEmail())){
+            this.funcionarioService.buscarPorEmail(funcionarioDto.getEmail())
+                    .ifPresent(func -> result.addError(new ObjectError("email", "Email já existente.")));
+            funcionario.setEmail(funcionarioDto.getEmail());
+        }
+
+        funcionario.setQtdHorasAlmoco(null);
+        funcionarioDto.getQtdHorasAlmoco().ifPresent(qtdHorasAlmoco -> funcionario.setQtdHorasAlmoco(Float.valueOf(qtdHorasAlmoco)));
+
+        funcionario.setQtdHorasTrabalhoDia(null);
+        funcionarioDto.getQtdHorasTrabalhoDia().ifPresent(qtdHorasTrabDia -> funcionario.setQtdHorasTrabalhoDia(Float.valueOf(qtdHorasTrabDia)));
+
+        funcionario.setValorHora(null);
+        funcionarioDto.getValorHora().ifPresent(valorHora -> funcionario.setValorHora(new BigDecimal(valorHora)));
+
+        if(funcionarioDto.getSenha().isPresent()){
+            funcionario.setSenha(PasswordUtils.gerarBCrypt(funcionarioDto.getSenha().get()));
+        }
+    }
 }
